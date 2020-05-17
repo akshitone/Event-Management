ae7f13aa35c336505271689e5c1519897cf9fab5
Akshit Mithaiwala <akshitone8@gmail.com>
2020-05-17 15:37:12 +0530

    client-side master page

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')    
+    path('', views.home, name='home'),
+    path('registration/', views.registration, name='registration'),
+    path('venue/', views.venue, name='venue')    
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    titlename = "Home | eVentes"
+    return render(request, 'client/home.html', {'titlename': titlename})
+
+def registration(request):
+    titlename = "Registration Form | eVentes"
+    return render(request, 'client/registration-form.html', {'titlename': titlename})
+
+def venue(request):
+    titlename = "Venue | eVentes"
+    return render(request, 'client/venue.html', {'titlename': titlename})
