145761870b65eed3dd48bd9f2700cd2f7b3f7e68
Viral Shsatri <shastriv19@gmail.com>
2020-08-20 17:34:08 +0530

    deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
