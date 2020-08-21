270d8ff5a71b5bb4e51672642cc402010d55a83c
Viral Shsatri <shastriv19@gmail.com>
2020-08-21 10:22:35 +0530

    deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/images')
+MEDIA_URL = '/static/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
